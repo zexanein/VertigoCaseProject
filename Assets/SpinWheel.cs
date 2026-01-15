7334447467be10797832877e3f468f97505c4dbc
Zexanein <draqgamedev@gmail.com>
2026-01-16 02:02:33 +0300

    Spin Animation

---
+++ Assets/SpinWheel.cs
@@ -0,0 +1,26 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpinWheel : MonoBehaviour
+{
+    [SerializeField] private Transform spinWheelTransform;
+    [SerializeField] private Button spinButton;
+    private float _rewardAngle = 45f;
+    private Tween _spinTween;
+    
+    private void Start()
+    {
+        spinButton.onClick.AddListener(Spin);
+    }
+    
+    private void Spin()
+    {
+        var randomSpins = Random.Range(3, 6);
+        var rotateAngle = randomSpins * 360f + _rewardAngle;
+        _spinTween?.Kill();
+        _spinTween =
+            spinWheelTransform.DORotate(new Vector3(0, 0, -rotateAngle), 2f, RotateMode.FastBeyond360)
+            .SetEase(Ease.InOutCubic);
+    }
+}
