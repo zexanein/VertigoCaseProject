1ec73a9d5d9f9788dd65affd1cd0e0dc2ab8c2ba
Zexanein <draqgamedev@gmail.com>
2026-01-16 10:58:09 +0300

    Spawning Reward Items

--- Assets/SpinWheel.cs
+++ Assets/SpinWheel.cs
@@ -5,20 +5,38 @@ using Random = UnityEngine.Random;
 
 public class SpinWheel : MonoBehaviour
 {
+    
+    private const int NumberOfRewards = 8;
+    private const float RewardRadius = 170;
+    private int RewardAngle => 360 / NumberOfRewards;
+    
     [SerializeField] private Transform spinWheelTransform;
     [SerializeField] private Button spinButton;
-    private float _rewardAngle = 45f;
+    [SerializeField] private GameObject rewardPrefab;
     private Tween _spinTween;
     
     private void Start()
     {
+        CreateRewards();
         spinButton.onClick.AddListener(Spin);
+    }
+
+    private void CreateRewards()
+    {
+        for (var i = 0; i < NumberOfRewards; i++)
+        {
+            var angle = i * RewardAngle;
+            var position = Quaternion.Euler(0, 0, angle) * Vector3.up * RewardRadius;
+            var reward = Instantiate(rewardPrefab, spinWheelTransform);
+            reward.transform.localPosition = position;
+            reward.transform.localRotation = Quaternion.Euler(0, 0, angle);
+        }
     }
     
     private void Spin()
     {
         var randomSpins = Random.Range(3, 6);
-        var rotateAngle = randomSpins * 360f + _rewardAngle;
+        var rotateAngle = randomSpins * 360f + RewardAngle;
         _spinTween?.Kill();
         _spinTween =
             spinWheelTransform.DORotate(new Vector3(0, 0, -rotateAngle), 2f, RotateMode.FastBeyond360)
