7e1e3f56fea36b8624cbdc9aa5c3e79520e64030
Zexanein <draqgamedev@gmail.com>
2026-01-19 04:44:16 +0300

    Bomb Index Bugfix

--- Assets/Scripts/SpinWheel.cs
+++ Assets/Scripts/SpinWheel.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
@@ -6,6 +5,10 @@ using Random = UnityEngine.Random;
 
 public class SpinWheel : MonoBehaviour
 {
+    [SerializeField] private Sprite bombSprite;
+    [SerializeField] private Transform spinWheelTransform;
+    [SerializeField] private SpinWheelItemDisplay itemDisplayPrefab;
+    
     private int _numberOfRewards;
     private int _bombIndex;
     private List<RewardInfo> _rewards;
@@ -13,13 +16,8 @@ public class SpinWheel : MonoBehaviour
     private Tween _spinTween;
     
     private int RewardAngle => 360 / _numberOfRewards;
-
-    [SerializeField] private Sprite bombSprite;
-    [SerializeField] private Transform spinWheelTransform;
-    [SerializeField] private SpinWheelItemDisplay itemDisplayPrefab;
     
     public delegate void SpinCompleteHandler(RewardInfo reward, bool isBomb);
-    
     
     public void GenerateRewards(RewardPool rewardPool, int numberOfRewards)
     {
@@ -30,22 +28,22 @@ public class SpinWheel : MonoBehaviour
         
         _numberOfRewards = numberOfRewards;
         
-        var bombIndex = Random.Range(0, _numberOfRewards);
+        _bombIndex = Random.Range(0, _numberOfRewards);
         var rewards = rewardPool.GetRandomRewards(_numberOfRewards - 1);
 
         _rewards = new List<RewardInfo>();
         
         for (var i = 0; i < _numberOfRewards; i++)
         {
-            _rewards.Add(i == bombIndex ? null : rewards[0]);
-            if (i != bombIndex) rewards.RemoveAt(0);
+            _rewards.Add(i == _bombIndex ? null : rewards[0]);
+            if (i != _bombIndex) rewards.RemoveAt(0);
         }
         
         for (var i = 0; i < _numberOfRewards; i++)
         {
             var createdItem = CreateItem(i);
             
-            if (i == bombIndex)
+            if (i == _bombIndex)
             {
                 createdItem.SetBombVisual(bombSprite);
                 continue;
@@ -73,7 +71,6 @@ public class SpinWheel : MonoBehaviour
         var earnedIndex = Random.Range(0, _numberOfRewards);
         var earnedIsBomb = earnedIndex == _bombIndex;
         var earnedReward = earnedIsBomb ? null : _rewards[earnedIndex];
-        
         var randomSpins = Random.Range(4, 7);
         var rotateAngle = earnedIndex * RewardAngle + randomSpins * 360;
         
